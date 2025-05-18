94f26a0b3bda93284b73482b52e742261953ffa9
joseph <josephtabia1@gmail.com>
2025-05-18 13:14:14 +0100

    handel space betwen zeros

--- read_map.c
+++ read_map.c
@@ -4,7 +4,6 @@ void	process_map_data(char *buff, t_game *game)
 {
 	game->map = ft_split(buff, '\n');
 }
-
 int	read_map(char *file, t_game *game)
 {
 	char	*buff;
@@ -32,7 +31,6 @@ int	read_map(char *file, t_game *game)
 		return (1);
 	return ((process_map_data(buff, game)), free(buff), 0);
 }
-
 int	check_map_wall(char **map)
 {
 	int	i;
@@ -43,26 +41,40 @@ int	check_map_wall(char **map)
 	if (!map || !map[0])
 		return (1);
 	raw_len = ft_strlen(map[0]);
+	if (raw_len == 0)
+		return (1);
 	j = 0;
 	while (j < raw_len)
 	{
-		if (map[0][j] != '1')
+        if(map[0][j] == 32)
+            j++;
+		else if(map[0][j] != '1')
 			return (1);
 		j++;
 	}
 	i = 1;
-	while (map[i])
+    j = 0;
+	while (map[i + 1] != NULL)
 	{
 		raw_len = ft_strlen(map[i]);
-		if (map[i][0] != '1' || map[i][ raw_len - 1] != '1')
+        if(map[i][j] == 32)
+        {
+            j++;
+            continue;
+        }
+		else if (raw_len == 0 || map[i][0] != '1' || map[i][raw_len - 1] != '1')
 			return (1);
 		i++;
 	}
 	j = 0;
-	raw_len = ft_strlen(map[i - 1]);
+	raw_len = ft_strlen(map[i]);  // map[i] is the last row after the loop
+	if (raw_len == 0)
+		return (1);
 	while (j < raw_len)
 	{
-		if (map[i - 1][j] != '1')
+        if(map[i][j] == 32)
+            j++;
+		else if (map[i][j] != '1')  // Check map[i], not map[i-1]
 			return (1);
 		j++;
 	}
@@ -89,27 +101,119 @@ void	err(char *str)
 	while (*str)
 		write(2, str++, 1);
 }
-int check_map_chars(char **map)
+int get_map_height(char **map)
+{
+    int height;
+
+    height = 0;
+    while(map[height])
+        height++;
+    return(height);
+}
+int count_player(char **map)
 {
     int i;
     int j;
+    int count_player;
 
     i = 0;
-    while (map[i])
+    count_player = 0;
+    while(map[i])
     {
         j = 0;
-        while (map[i][j])
+        while(map[i][j])
         {
-            if (map[i][j] == 32 && map[i][j + 1] == '0' && map[i][j - 1] == '0')
-				map[i][j] = '0';
-            if (map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'N'
-                && map[i][j] != 'S' && map[i][j] != 'E' && map[i][j] != 'W')
+            if(map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'E' || map[i][j] == 'W')
+                count_player++;
+            j++;
+        }
+        i++;
+    }
+    return(count_player);
+}
+int is_valid_char(char c)
+{
+    return(c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E' || c == 'W' || c == ' ');
+}
+int horizonal_zeros(char *row, int j, int row_len)
+{
+    if(j > 0 && j < row_len - 1)
+        return(row[j - 1] == '0' && row[j + 1] == '0');
+    return(0);
+}
+int vertical_zeros(char **map, int i, int j, int map_height)
+{
+
+    int prev_len;
+    int next_len;
+    if(i > 0 && i < map_height - 1)
+    {
+        prev_len = ft_strlen(map[i - 1]);
+        next_len = ft_strlen(map[i + 1]);
+        if(j < prev_len && j < next_len)
+            return(map[i - 1][i] == '0' && map[i + 1][j] == '0');
+    }
+    return(0);
+}
+void convert_space_to_zero(char **map)
+{
+    int i;
+    int j;
+    int row_len;
+    int map_height;
+
+    map_height = get_map_height(map);
+    i = 0;
+    while(map[i])
+    {
+        j = 0;
+        row_len = ft_strlen(map[i]);
+        while(i < row_len)
+        {
+            if(map[i][j] == ' ')
             {
-                return (1);
+                if(horizonal_zeros(map[i], j, row_len))
+                    map[i][j] = '0';
+                else if(vertical_zeros(map, i, j, map_height))
+                    map[i][j] = '0';
             }
             j++;
         }
         i++;
     }
+}
+int validate_map_char(char **map)
+{
+    int i;
+    int j;
+    int row_len;
+
+    i = 0;
+    while(map[i])
+    {
+        j = 0;
+        row_len = ft_strlen(map[i]);
+        while(j < row_len)
+        {
+            if(!is_valid_char(map[i][j]))
+                return(1);
+            j++;
+        }
+        i++;
+    }
+    return(0);
+}
+int check_map_chars(char **map)
+{
+    int player_count;
+
+    if(!map || !map[0])
+        return(1);
+    convert_space_to_zero(map);
+    if(validate_map_char(map))
+        return(1);
+    player_count = count_player(map);
+    if(player_count != 1)
+        return(1);
     return (0);
 }
